367abd49dab7643f704db2c9bcfc171a769baa8c
Shivam Kumar Singh <getshivamsingh44@gmail.com>
2026-07-18 17:09:22 +0530

    Initial Commit

---
+++ SwiftDidLoadProj/SwiftDidLoadProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDidLoadProjApp.swift
+//  SwiftDidLoadProj
+//
+//  Created by shivam kumar singh on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDidLoadProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
